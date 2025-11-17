017c6e23b06566ad087e44b84456b76f8260bd4c
Muhammad Rehan Alam <rehan@beebolt.com>
2025-11-17 10:53:45 +0500

    chore(ui): fixes ui issues

--- src/app/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(dashboard)/organization/[organizationId]/page.tsx
@@ -7,7 +7,7 @@ export default async function OrganizationPage({ params }: PageProps) {
   const { organizationId } = await params;
 
   return (
-    <div className=" w-full flex flex-col items-center">
+    <div className=" w-full flex flex-col items-center mt-12">
       <div className="max-w-3/4 flex items-center justify-between mx-auto ">
         <WelcomeCallout organizationId={organizationId} />
       </div>
